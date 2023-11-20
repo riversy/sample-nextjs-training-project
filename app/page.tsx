26ddf97798e844cff2533ec60559d13c5fe74caf
Igor Goltsov <riversy@gmail.com>
2023-11-20 16:45:27 +0300

    Add stylelint

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import styles from './page.module.css';
 import {PTag} from "@/components";
 import {JSX} from "react";
 
 export default function Home(): JSX.Element {
   return (
-    <main className={styles.main}>
+    <main>
       <PTag>Test text</PTag>
     </main>
   );
